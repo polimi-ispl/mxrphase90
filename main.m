1dbf27be4dfaeb4b252688ee2f6d3621d62a2330
Riccardo Giampiccolo <79471976+riccardogiampiccolo@users.noreply.github.com>
2024-03-08 11:41:20 +0100

    Update main.m

--- main.m
+++ main.m
@@ -1,6 +1,6 @@
 clear; close all; clc; 
 
-%% Copyright (C) 2024 Riccardo Giampiccolo
+%% Copyright (C) 2024 Riccardo Giampiccolo, Samuele Del Moro, Claudio Eutizi, Mattia Massimi
 %
 % Permission is hereby granted, free of charge, to any person obtaining a copy
 % of this software and associated documentation files (the "Software"), to deal
